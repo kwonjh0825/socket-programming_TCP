df75d87f8bfd0697dbf0dc45f5a08e72314522e1
권준혁 <kwonjh0825@kwonjh0825.local>
2022-09-23 16:22:51 +0900

    second commit

--- plus_minus_server.py
+++ plus_minus_server.py
@@ -7,28 +7,27 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((serverName, serverPort))
 serverSocket.listen(1)
 
+connectionSocket, addr = serverSocket.accept()
+
 result = 0
 r = 0
-
 oper = ''
 
 while True:
-    connectionSocket, addr = serverSocket.accept()
-    print('1')
     string = connectionSocket.recv(1024).decode()
-    print(string)
+
     for i in string:
         if i == '+' or i == '-':
             oper = i
             idx = string.index(i)
-    print(int(string[0:idx]), oper, int(string[idx+1:]))
 
     if oper == '+':
             r = int(string[0:idx]) + int(string[idx+1:])
     elif oper == '-':
             r = int(string[0:idx]) - int(string[idx+1:])
+
+    print(f"{string} = {r}")
+    
     result = str(r)
+    
     connectionSocket.send(result.encode())
-
-
-
--- server_tcp.py
+++ server_tcp.py
@@ -10,31 +10,30 @@ serverSocket.listen(1)
 
 connectionSocket, addr = serverSocket.accept()
 
-
-
-calSocket = socket(AF_INET, SOCK_STREAM)
+sumSocket = socket(AF_INET, SOCK_STREAM)
+mulSocket = socket(AF_INET, SOCK_STREAM)
 
 result = ''
+
+sumSocket.connect((serverName, 9998))       # plus_minus_server Port: 9998
+mulSocket.connect((serverName, 9999))       # times_div_server Port: 9999
 
 while True:
     string = connectionSocket.recv(1024).decode()
     if string == 'q':
         break
     else:
-        if '+' in string or '-' in string:                        # send to plus_minus_server
-            calSocket.connect((serverName, 9998))       # plus_minus_server Port: 9998
-            calSocket.send(string.encode())
+        if '+' in string or '-' in string:              # send to plus_minus_server
+            sumSocket.send(string.encode())
 
-            result = calSocket.recv(1024).decode()
-            calSocket.close()                
-                
-        elif '*' in string or '/' in string:
-            calSocket.connect((serverName, 9999))       # times_div_server Port: 9999
-            calSocket.send(string.encode())
+            result = sumSocket.recv(1024).decode()
+        elif '*' in string or '/' in string:            # send to times_div_server
+            mulSocket.send(string.encode())
 
-            result = calSocket.recv(1024).decode()
-            calSocket.close()
-
+            result = mulSocket.recv(1024).decode()
+            
         connectionSocket.send(result.encode())
-
+        
+sumSocket.close()                
+mulSocket.close()
 connectionSocket.close()
